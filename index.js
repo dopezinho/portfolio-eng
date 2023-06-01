ef929c736b10eda4d0b1bbe01475574505607230
fezinho <opugnero@gmail.com>
2023-06-01 10:46:14 -0300

    contact and home ok

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-function clickbtn() {
-    let centerContent = document.getElementById("center-content")
-    carregar_arquivo("contact.html", centerContent)
+let centerContent = document.getElementById("center-content")
+
+/*  LOAD HOME  */
+
+function clickbtnHome() {
+    loadHome("home.html", centerContent)
 }
 
-function carregar_arquivo(url, Xelement) {
+function loadHome(url, Xelement) {
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status ==200) {
+            Xelement.innerHTML = xmlhttp.responseText
+        }
+    }
+    xmlhttp.open("GET", url)
+    xmlhttp.send()
+}
+
+/*  LOAD CONTACT  */
+
+function clickbtnContact() {
+    loadContact("contact.html", centerContent)
+}
+
+function loadContact(url, Xelement) {
     if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest()
     }
